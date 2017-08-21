e22e776c64ae30587b3f432a935b3b3fd294da81
Shiny Shih <sekei@shixinningde-MacBook-Pro.local>
2017-08-21 14:02:12 +0800

    doraemon color

---
+++ theme/DoraemonViewController.swift
@@ -0,0 +1,44 @@
+//
+//  DoraemonViewController.swift
+//  theme
+//
+//  Created by 施馨檸 on 2017/8/21.
+//  Copyright © 2017年 施馨檸. All rights reserved.
+//
+
+import UIKit
+
+class DoraemonViewController: UIViewController {
+
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var doraemonColorView: UIView!
+    
+    @IBAction func changeColor(_ sender: Any) {
+        doraemonColorView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
